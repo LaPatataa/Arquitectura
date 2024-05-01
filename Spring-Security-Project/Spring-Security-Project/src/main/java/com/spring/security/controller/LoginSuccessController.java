85971ed195ab2d1641e94538b28bf61bdcb4473e
manuel <manuelrondon25@outlook.com>
2024-05-01 17:47:52 -0500

    Ultimo

---
+++ Spring-Security-Project/Spring-Security-Project/src/main/java/com/spring/security/controller/LoginSuccessController.java
@@ -0,0 +1,16 @@
+package com.spring.security.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginSuccessController {
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(HttpServletRequest request, HttpServletResponse response) {
+        // Lógica adicional si es necesario
+        return "redirect:/home/index";
+    }
+}
