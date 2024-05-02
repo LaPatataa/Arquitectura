3a26d92a4c92ed041045d1f71e90458662923565
santiagoescarpeta <tomyplay0@gmail.com>
2024-05-01 20:30:39 -0500

    Agregado archivo de

--- Spring-Security-Project/Spring-Security-Project/src/main/java/com/spring/security/config/cors.java
+++ Spring-Security-Project/Spring-Security-Project/src/main/java/com/spring/security/config/cors.java
@@ -5,15 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
+public class cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedOrigins("http://localhost:4200") // Permitir solo desde localhost:4200
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*"); // Cabeceras permitidas
     }
 }
